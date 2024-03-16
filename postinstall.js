e8742cfe4a2e495a141685f2053479cab77dbbee
Deepak Singh <webdeveloperdeepaksingh@gmail.com>
2024-03-16 13:32:35 +0530

    delete postinstall

--- postinstall.js
+++
@@ -1,6 +0,0 @@
-import { emptyDirSync, copySync } from 'fs-extra';
-import { join } from 'path';
-
-const topDir = __dirname;
-emptyDirSync(join(topDir, 'public', 'tinymce'));
-copySync(join(topDir, 'node_modules', 'tinymce'), join(topDir, 'public', 'tinymce'), { overwrite: true });
